dc1d9d942e7c1f3a7acaa98aae7b1537c3bf268b
JcAntoine <jcantoine@live.fr>
2017-12-16 13:01:22 +0100

    Création des menus
    Il reste plus qu'à instantier les pokemons en fonction des sliders

---
+++ MiniPokemon/Assets/Classes/GlobalControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalControl : MonoBehaviour {
+
+    public static GlobalControl Instance;
+
+    public int pokemon1;
+    public int pokemon2;
+    public int pokemon3;
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
